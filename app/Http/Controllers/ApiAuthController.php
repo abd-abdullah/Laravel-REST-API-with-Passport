f6e34751c47cdc0e3762ea7491b4d8d78e70a58a
Md. Abdullah <abdullah@systechdigital.com>
2019-11-25 02:24:40 +0600

    update api login system

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use GuzzleHttp\Client;
+use App\User;
 use Illuminate\Http\Request;
+use Qauth;
 
 class ApiAuthController extends Controller
 {
@@ -12,18 +14,24 @@ class ApiAuthController extends Controller
     {
     $this->validateLogin($request);
 
-    $http = new Client;
+    $oauth = User::where(['users.email' => $request->client_email, 'oauth_clients.secret' => $request->client_secret])->join('oauth_clients','users.id', '=', 'oauth_clients.user_id')->select('oauth_clients.secret', 'oauth_clients.id')->orderBy('oauth_clients.created_at', 'DESC')->first();
 
-    $response = $http->post(url('oauth/token'), [
-        'form_params' => [
-            'grant_type' => 'password',
-            'client_id' => '3',
-            'client_secret' => 'HOI7DINvHqhdpWjR3okcrD3KZG26l8W2mc0xkr58',
-            'username' => $request->email,
-            'password' => $request->password,
-            'scope' => '',
-        ],
-    ]);
+    if($oauth != NULL){
+        $http = new Client;
+        $response = $http->post(url('oauth/token'), [
+            'form_params' => [
+                'grant_type' => 'password',
+                'client_id' => $oauth->id,
+                'client_secret' => $oauth->secret,
+                'username' => $request->email,
+                'password' => $request->password,
+                'scope' => '',
+            ],
+        ]);
+    }
+    else{
+        $response = "Invalid input";
+    }
 
     return json_decode((string) $response->getBody(), true);
 }
@@ -39,8 +47,10 @@ class ApiAuthController extends Controller
     protected function validateLogin(Request $request)
     {
         $request->validate([
-            'email' => 'required|string',
+            'email' => 'required|email',
             'password' => 'required|string',
+            'client_email' => 'required|email',
+            'client_secret' => 'required|string',
         ]);
     }
 }
